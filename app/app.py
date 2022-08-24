cb48f6e6036c9ab63e5bff495e8c966707a19a92
sekhar76405 <ssjio5402@gmail.com>
2022-08-24 20:55:16 +0530

    Basic flask app

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+        
+@app.route("/time")
+def print_time():
+    date = str(datetime.now())
+    time = date[10:19]
+    return time
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
